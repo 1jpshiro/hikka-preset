445fdd4c35778afb1477842d9ac678318a0c1cf5
jpshiro <162577448+1jpshiro@users.noreply.github.com>
2024-07-02 17:49:30 +0700

    .

--- hikka_info.py
+++ hikka_info.py
@@ -175,8 +175,8 @@ class HikkaInfoMod(loader.Module):
                 **(
                     {"photo": self.config["banner_url"]}
                     if self.config["banner_url"] and (
-                        file.webpage.type != "gif" and (chat_gif.default_banned_rights.send_gifs is False if isinstance(chat_gif, Channel) else True)
-                        or file.webpage.type == "gif" and (chat_gif.default_banned_rights.send_gifs is True if isinstance(chat_gif, Channel) else True)
+                        file.webpage.type != "gif" and ((chat_gif.default_banned_rights.send_gifs is False) if isinstance(chat_gif, Channel) else True)
+                        or file.webpage.type == "gif" and ((chat_gif.default_banned_rights.send_gifs is True) if isinstance(chat_gif, Channel) else True)
                     )
                     else {}
                 ),
